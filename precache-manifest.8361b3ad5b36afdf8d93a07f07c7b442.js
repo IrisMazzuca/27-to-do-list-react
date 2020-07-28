4e2bd2f7876bd7edfae47c2138715a21558862d5
Iris Mazzuca <irismazzuca@gmail.com>
2020-07-27 23:46:39 -0300

    Updates

---
+++ precache-manifest.8361b3ad5b36afdf8d93a07f07c7b442.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "846744d5af4f9030b72773d79ef8ba95",
+    "url": "/to-do-list-react/index.html"
+  },
+  {
+    "revision": "a1c90605def584a5a539",
+    "url": "/to-do-list-react/static/css/main.da60b492.chunk.css"
+  },
+  {
+    "revision": "0b98409b156b1e8a7c4b",
+    "url": "/to-do-list-react/static/js/2.c5218cfe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/to-do-list-react/static/js/2.c5218cfe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1c90605def584a5a539",
+    "url": "/to-do-list-react/static/js/main.67026df8.chunk.js"
+  },
+  {
+    "revision": "67502b13f713af20994a",
+    "url": "/to-do-list-react/static/js/runtime-main.41cf4c21.js"
+  }
+]);
